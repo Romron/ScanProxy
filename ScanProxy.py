953ebe992713f6963b44686c839cf94c5532f80c
Romron <romron@ukr.net>
2020-08-06 20:32:48 +0300

    Первый пробный commit

---
+++ ScanProxy.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import re 
+import json
+
+
+
+# r'((?:[0-9]{1,5}\.){3}[0-9]{1,3}(?::[0-9]{2,5})?)'
+#    00000.00000.00000.  000        : 00000
+#    									80
+#    									443
+
+
